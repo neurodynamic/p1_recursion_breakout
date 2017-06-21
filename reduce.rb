ed5200e54d9e7b723317806b17ad629ea675d324
Mark Stewart <markstewart@dbc-markstewart.local>
2017-06-21 08:23:53 -0700

    solved

--- reduce.rb
+++ reduce.rb
@@ -1,4 +1,19 @@
 def reduce(array, starting_value = nil)
+  if starting_value
+    memo = starting_value
+    rest_of_array = array
+  else
+    memo = array[0]
+    rest_of_array = array[1..-1]
+  end
+  
+  index = 0
+  while index < rest_of_array.length do
+    memo = yield(memo, rest_of_array[index])
+    index += 1
+  end
+
+  memo
 end
 
 
--- string_operations.rb
+++ string_operations.rb
@@ -1,4 +1,7 @@
 def reverse(string)
+  return "" if string.empty?
+
+  reverse(string[1..-1]) + string[0]
 end
 
 output = reverse("hello")
